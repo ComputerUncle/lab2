c2bc67141b3939aa1b04d73128b6f934ea5d66fc
ComputerUncle <mailtojovichua@gmail.com>
2025-10-22 11:23:12 +0800

    Some BMI code

--- code.py
+++ code.py
@@ -1,1 +1,4 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+calculate_bmi(weight=57, height=1.73)
