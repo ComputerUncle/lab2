969681cce26d3cff71bc79defd315fe6f2c67aa5
ComputerUncle <mailtojovichua@gmail.com>
2025-10-29 10:03:35 +0800

    Updated python code

--- code.py
+++ code.py
@@ -1,4 +1,16 @@
 def calculate_bmi(height, weight):
  print("Height = " + str(height))
  print("Weight = " + str(weight))
-calculate_bmi(weight=57, height=1.73)
+ bmi = weight/(height*height)
+ print(str(bmi))
+ if bmi < 18.5:
+  print ("User is under weight")
+ elif bmi >= 18.5 and bmi <= 25.0:
+  print ("User is normal weight")
+ elif bmi > 25.0:
+  print ("User is over weight")
+
+weight = float(input())
+height = float(input())
+calculate_bmi(weight, height)
+
