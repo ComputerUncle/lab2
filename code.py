b03afc0472baf5d9ff07e81fb41c72f046e3989a
ComputerUncle <mailtojovichua@gmail.com>
2025-11-04 00:12:19 +0800

    Updated function with return

--- code.py
+++ code.py
@@ -5,10 +5,13 @@ def calculate_bmi(height, weight):
  print(str(bmi))
  if bmi < 18.5:
   print ("User is under weight")
+  return -1
  elif bmi >= 18.5 and bmi <= 25.0:
   print ("User is normal weight")
+  return 0
  elif bmi > 25.0:
   print ("User is over weight")
+  return 1
 
 weight = float(input())
 height = float(input())
